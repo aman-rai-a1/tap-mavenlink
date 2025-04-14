722b1d23726462f0379b9d963f3a2d419418e9c5
Aman Rai <104914320+aman-rai-a1@users.noreply.github.com>
2025-04-14 18:16:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-mavenlink',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_mavenlink'],
       install_requires=[
-          'tap-framework==0.0.4',
+          'tap-framework==0.2.0',
       ],
       entry_points='''
           [console_scripts]
